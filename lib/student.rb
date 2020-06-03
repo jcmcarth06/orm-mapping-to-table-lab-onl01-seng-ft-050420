1f0619dba5e53e6e2ad94fe9c9a67e3bed732b02
Joshua McArthur <jcmcarth06@gmail.com>
2020-06-03 21:02:37 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,17 @@
 class Student
+ attr_accessor :name, :grade
+ attr_reader :id
+ 
+ def initialize(name, grade, id=nil)
+   @name = name
+   @grade = grade
+   @id = id
+ end
 
+ def self.create_tavle
+   sql = <<-SQL
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
+  end
   
 end
